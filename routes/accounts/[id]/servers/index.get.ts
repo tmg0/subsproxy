15ba8ed183aaa42e0b01cebb1d36f0a337795707
zekun.jin <zekun.jin@connext.com.cn>
2023-06-29 14:57:50 +0800

    feat: add server ping

--- routes/accounts/[id]/servers/index.get.ts
+++ routes/accounts/[id]/servers/index.get.ts
@@ -1,14 +1,43 @@
 import { z } from 'zod'
+import ping from 'ping'
 import { prisma } from '~/utils/prisma'
 
 const Query = z.object({
   encode: z.boolean()
 })
 
+const pingServers = async (servers: { address: string }[]) => {
+  const VMESS_PREFIX = 'vmess://'
+  const SS_PREFIX = 'ss://'
+
+  const hosts: string[] = servers.map(({ address }) => {
+    if (address.startsWith(VMESS_PREFIX)) {
+      const decode = atob(address.replace(VMESS_PREFIX, ''))
+      return JSON.parse(decode).add
+    }
+
+    if (address.startsWith(SS_PREFIX)) {
+      const [encode] = address.replace(SS_PREFIX, '').split('#')
+      const [_, server] = atob(encode).split(':')
+      return server.split('@')[1]
+    }
+
+    return address
+  })
+  return !(await (Promise.all(hosts.map(host => ping.promise.probe(host))))).map(alive => alive).includes(false)
+}
+
 export default defineEventHandler(async (event) => {
   const { id } = getRouterParams(event)
   const query: z.infer<typeof Query> = getQuery(event)
   const serverIds = await prisma.accountServer.findMany({ where: { accountId: id } })
   const servers = await prisma.server.findMany({ where: { OR: serverIds.map(({ serverId }) => ({ id: serverId })) } })
-  return query.encode ? btoa(servers.map(({ address }) => address).join('\n')) : servers
+
+  pingServers(servers).then((valid) => {
+    if (!valid) {
+      // TODO: Refetch subscriptions.
+    }
+  })
+
+  return query.encode ? btoa(servers.map(({ address }) => address).join('\n')) : servers.map(({ id }) => ({ id }))
 })
--- routes/subscriptions/[id]/index.put.ts
+++ routes/subscriptions/[id]/index.put.ts
@@ -6,9 +6,11 @@ export default defineEventHandler(async (event) => {
   const subsctiption = await prisma.subsctiption.findUnique({ where: { id } })
   const servers = atob(await ofetch(subsctiption.address)).split(/[\n\r]/).filter(Boolean).flat()
 
+  const createServers = servers.map(address => prisma.server.create({ data: { address, subscriptionId: subsctiption.id } }))
+
   await prisma.$transaction([
-    ...servers.map(address => prisma.server.create({ data: { address, subscriptionId: subsctiption.id } })),
-    prisma.server.deleteMany({ where: { subscriptionId: subsctiption.id } })
+    prisma.server.deleteMany({ where: { subscriptionId: subsctiption.id } }),
+    ...createServers
   ])
 
   return subsctiption
