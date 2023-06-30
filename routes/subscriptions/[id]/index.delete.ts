15388c7f24dc891d6e5be94615314518985806ad
zekunjin <zekun.jin@foxmail.com>
2023-06-30 13:34:22 +0800

    feat: del subscription

--- routes/subscriptions/[id]/index.delete.ts
+++ routes/subscriptions/[id]/index.delete.ts
@@ -1,6 +1,17 @@
 import { prisma } from '~/utils/prisma'
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   const { id } = getRouterParams(event)
+  const servers = prisma.server.findMany({ where: { subscriptionId: id } })
+
+  await prisma.$transaction([
+    servers.map(({ id: serverId }) => [
+      prisma.server.deleteMany({ where: { subscriptionId: id } }),
+      prisma.accountServer.deleteMany({ where: { serverId } })
+    ]),
+
+    prisma.accountSubscription.deleteMany({ where: { subscriptionId: id } })
+  ].flat())
+
   return prisma.subsctiption.delete({ where: { id } })
 })
