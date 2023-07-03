4b66452fbe37dc0bfbb7ff083722d587561dc1fc
zekunjin <zekun.jin@foxmail.com>
2023-07-03 11:33:12 +0800

    chore: fetch all subs

--- routes/subscriptions/index.get.ts
+++ routes/subscriptions/index.get.ts
@@ -1,12 +1,5 @@
-import { z } from 'zod'
 import { prisma } from '~/utils/prisma'
 
-const Query = z.object({
-  address: z.string()
-})
-
-export default defineAuthenticatedEventHandler((event) => {
-  const query: z.infer<typeof Query> = getQuery(event)
-  Query.parse(query)
-  return prisma.subscription.findMany({ where: { address: query.address } })
+export default defineAuthenticatedEventHandler(() => {
+  return prisma.subscription.findMany()
 })
