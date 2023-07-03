0eb43e57fb9237261ae1c04acfb5169750e91f11
zekunjin <zekun.jin@foxmail.com>
2023-07-03 11:20:14 +0800

    chore: exclude password field

--- routes/accounts/index.get.ts
+++ routes/accounts/index.get.ts
@@ -1,5 +1,5 @@
 import { prisma } from '~/utils/prisma'
 
 export default defineAuthenticatedEventHandler(() => {
-  return prisma.account.findMany()
+  return prisma.account.findMany({ select: { id: true, username: true, password: false, createdAt: true, updatedAt: true } })
 })
