1e0818170ee96b2d27bacb8a7f213dcc1ba123f4
zekunjin <zekun.jin@foxmail.com>
2023-07-12 13:20:31 +0800

    fix: unbind subs err

--- routes/accounts/[id]/subscriptions/index.delete.ts
+++ routes/accounts/[id]/subscriptions/index.delete.ts
@@ -1,6 +1,9 @@
 import { prisma } from '~/utils/prisma'
 
-export default defineAuthenticatedEventHandler((event) => {
+export default defineAuthenticatedEventHandler(async (event) => {
   const { id } = getRouterParams(event)
+  const subscriptions = await prisma.accountSubscription.findMany({ where: { accountId: id } })
+  const servers = await prisma.server.findMany({ where: { OR: subscriptions.map(({ subscriptionId }) => ({ subscriptionId })) } })
+  await prisma.accountServer.deleteMany({ where: { OR: servers.map(({ id }) => ({ serverId: id })) } })
   return prisma.accountSubscription.deleteMany({ where: { accountId: id } })
 })
--- routes/accounts/[id]/subscriptions/index.get.ts
+++ routes/accounts/[id]/subscriptions/index.get.ts
@@ -2,7 +2,7 @@ import { prisma } from '~/utils/prisma'
 
 export default defineAuthenticatedEventHandler((event) => {
   const id = getRouterParam(event, 'id')
-  return prisma.subscription.findMany({
-    include: { accountSubscription: { where: { accountId: id } } }
+  return prisma.accountSubscription.findMany({
+    where: { accountId: id }
   })
 })
