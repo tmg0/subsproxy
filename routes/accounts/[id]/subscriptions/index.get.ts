538688433e687401bd644c2543a42bd424f8cfdb
zekunjin <zekun.jin@foxmail.com>
2023-07-12 15:18:35 +0800

    chore: include server / account fields

--- routes/accounts/[id]/subscriptions/index.get.ts
+++ routes/accounts/[id]/subscriptions/index.get.ts
@@ -3,6 +3,16 @@ import { prisma } from '~/utils/prisma'
 export default defineAuthenticatedEventHandler((event) => {
   const id = getRouterParam(event, 'id')
   return prisma.accountSubscription.findMany({
-    where: { accountId: id }
+    where: { accountId: id },
+    include: {
+      subscription: {
+        include: {
+          server: true,
+          accountSubscription: {
+            include: { account: { select: { username: true } } }
+          }
+        }
+      }
+    }
   })
 })
