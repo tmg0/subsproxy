e7a8cff41a49fa8d703b203ef30f4d9d93d0d1d6
zekunjin <zekun.jin@foxmail.com>
2023-07-14 11:53:44 +0800

    feat: del account relations

--- routes/accounts/[id]/index.delete.ts
+++ routes/accounts/[id]/index.delete.ts
@@ -4,6 +4,8 @@ export default defineEventHandler((event) => {
   const id = getRouterParam(event, 'id')
   return prisma.$transaction([
     prisma.device.deleteMany({ where: { accountId: id } }),
+    prisma.accountServer.deleteMany({ where: { accountId: id } }),
+    prisma.accountSubscription.deleteMany({ where: { accountId: id } }),
     prisma.account.delete({ where: { id } })
   ])
 })
