8fa80d1a6849d19943fe6086f4e3e2388aa5b922
zekunjin <zekun.jin@foxmail.com>
2023-10-10 09:53:17 +0800

    chore: reject unauthorized

--- utils/minio.ts
+++ utils/minio.ts
@@ -1,4 +1,7 @@
+import { Agent } from 'node:https'
 import { Client } from 'minio'
+import { extname } from 'pathe'
+import { nanoid } from 'nanoid'
 
 interface MinioUploadOptions {
   bucket: string
@@ -7,14 +10,15 @@ interface MinioUploadOptions {
 
 const runtimeConfig = process.env
 
-const HOST = `http://${runtimeConfig.MINIO_END_POINT}:${runtimeConfig.MINIO_PORT}`
+const HOST = `https://${runtimeConfig.MINIO_END_POINT}:${runtimeConfig.MINIO_PORT}`
 
 const minioClient = new Client({
   endPoint: runtimeConfig.MINIO_END_POINT,
   port: Number(runtimeConfig.MINIO_PORT),
-  useSSL: false,
+  useSSL: true,
   accessKey: runtimeConfig.MINIO_ACCESS_KEY,
-  secretKey: runtimeConfig.MINIO_SECRET_KEY
+  secretKey: runtimeConfig.MINIO_SECRET_KEY,
+  transportAgent: new Agent({ rejectUnauthorized: false })
 })
 
 export const upload = async (file: Buffer, options: MinioUploadOptions) => {
@@ -23,9 +27,11 @@ export const upload = async (file: Buffer, options: MinioUploadOptions) => {
   const isBucketExist = await minioClient.bucketExists(options.bucket)
   if (!isBucketExist) { return }
 
+  const filename = nanoid() + extname(options.filename)
+
   return new Promise((resolve, reject) => {
-    minioClient.putObject(options.bucket, options.filename, file, (error) => {
-      !error ? resolve(`${HOST}/${options.bucket}/${options.filename}`) : reject(error)
+    minioClient.putObject(options.bucket, filename, file, (error) => {
+      !error ? resolve(`${HOST}/${options.bucket}/${filename}`) : reject(error)
     })
   })
 }
