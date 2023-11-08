4bf49a6c2d0c536fbbaa139631f4e21ee0314ba3
zekunjin <zekun.jin@foxmail.com>
2023-11-08 16:41:58 +0800

    chore: add openapi support

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,6 +3,10 @@ import { defineNitroConfig } from 'nitropack/config'
 export default defineNitroConfig({
   alias: { '~': __dirname },
 
+  experimental: {
+    openAPI: true
+  },
+
   devServer: {
     watch: ['routes/**/*.ts']
   }
